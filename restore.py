23f45e6123b46383b5fa003679e2ffb1665adf1e
EhsanJahanbakhsh <ehsanjahanbakhsh9@gmail.com>
2024-11-06 22:32:57 +0330

    Update restore.py
    fix port number for restore action

--- restore.py
+++ restore.py
@@ -13,9 +13,9 @@ def restore_database(db_config, backup_file_name):
     try:
 
         if db_config['type'] == 'postgresql':
-            command = f"PGPASSWORD={db_config['password']} psql -h {db_config['host']} -U {db_config['user']} -d {db_config['database']} -f {download_path}"
+            command = f"PGPASSWORD={db_config['password']} psql -h {db_config['host']} -p {db_config['port']} -U {db_config['user']} -d {db_config['database']} -f {download_path}"
         elif db_config['type'] == 'mysql':
-            command = f"mysql -h {db_config['host']} -u {db_config['user']} -p{db_config['password']} {db_config['database']} < {download_path}"
+            command = f"mysql -h {db_config['host']} -P {db_config['port']} -u {db_config['user']} -p{db_config['password']} {db_config['database']} < {download_path}"
         subprocess.run(command, shell=True, check=True)
         print(f"Database restored from {backup_file_name}")
     except subprocess.CalledProcessError as err:
